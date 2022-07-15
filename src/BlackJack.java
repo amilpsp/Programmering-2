433201a2cd545ef9611cdac6bca7364ad542e129
amilpsp <amanda.m.ruiz@gmail.com>
2022-07-15 16:12:50 +0200

    Turned myDeck.allCards from an ArrayList into a Queue to be able to draw cards and put them at the back of the deck. Imma have a counter, so that after drawing cards 52 times it automatically shuffles the deck again (and lets the user know that).

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BlackJack {
     public static void main (String[]args){
         new PokerDeck();
         PokerDeck.Deck myDeck = new PokerDeck.Deck();
         myDeck.generateCards();
         myDeck.shuffleDeck();
+
+        Queue<PokerDeck.Card> playableCards = new LinkedList<>(Arrays.asList(myDeck.AllCards.toArray(new PokerDeck.Card[52])));
+        System.out.println(playableCards);
+
+
     }
 }
--- src/PokerDeck.java
+++ src/PokerDeck.java
@@ -1,74 +1,6 @@
 import java.util.*;
 public class PokerDeck {
     static class Deck extends PokerDeck {
-        enum Color{
-            Diamonds,
-            Spades,
-            Hearts,
-            Clubs,
-        }
-        class Card extends Deck {
-            private int cardsValue;
-            private Color cardsColor;
-
-            public Card(Color aColor, int aValue){  //Card constructor
-                this.cardsColor = aColor;
-                this.cardsValue = aValue;
-            } //Constructor
-            public int getCardsValue(){
-                return cardsValue;
-            }
-            public Color getCardsColor(){
-                return cardsColor;
-            }
-            public String toString(){
-                String valueToString = null;
-
-                switch(this.cardsValue){
-                    case 2:
-                        valueToString="Two";
-                        break;
-                    case 3:
-                        valueToString="Three";
-                        break;
-                    case 4:
-                        valueToString="Four";
-                        break;
-                    case 5:
-                        valueToString="Five";
-                        break;
-                    case 6:
-                        valueToString="Six";
-                        break;
-                    case 7:
-                        valueToString="Seven";
-                        break;
-                    case 8:
-                        valueToString="Eight";
-                        break;
-                    case 9:
-                        valueToString="Nine";
-                        break;
-                    case 10:
-                        valueToString="Ten";
-                        break;
-                    case 11:
-                        valueToString="Jack";
-                        break;
-                    case 12:
-                        valueToString="Queen";
-                        break;
-                    case 13:
-                        valueToString="King";
-                        break;
-                    case 14:
-                        valueToString="Ace";
-                        break;
-                }
-                return valueToString + " of " + cardsColor.toString();
-            }
-
-        }
         List<Card> AllCards = new ArrayList<>();
 
     //For simplicity's sake I'm just going to have one deck (only 52 cards)
@@ -91,5 +23,75 @@ public class PokerDeck {
         }
 
     }
+    enum Color {
+        Diamonds,
+        Spades,
+        Hearts,
+        Clubs,
+    }
+
+    class Card extends Deck {
+        private int cardsValue;
+        private Color cardsColor;
+
+        public Card(Color aColor, int aValue){  //Card constructor
+            this.cardsColor = aColor;
+            this.cardsValue = aValue;
+        } //Constructor
+        public int getCardsValue(){
+            return cardsValue;
+        }
+        public Color getCardsColor(){
+            return cardsColor;
+        }
+        public String toString(){
+            String valueToString = null;
+
+            switch(this.cardsValue){
+                case 2:
+                    valueToString="Two";
+                    break;
+                case 3:
+                    valueToString="Three";
+                    break;
+                case 4:
+                    valueToString="Four";
+                    break;
+                case 5:
+                    valueToString="Five";
+                    break;
+                case 6:
+                    valueToString="Six";
+                    break;
+                case 7:
+                    valueToString="Seven";
+                    break;
+                case 8:
+                    valueToString="Eight";
+                    break;
+                case 9:
+                    valueToString="Nine";
+                    break;
+                case 10:
+                    valueToString="Ten";
+                    break;
+                case 11:
+                    valueToString="Jack";
+                    break;
+                case 12:
+                    valueToString="Queen";
+                    break;
+                case 13:
+                    valueToString="King";
+                    break;
+                case 14:
+                    valueToString="Ace";
+                    break;
+            }
+            return valueToString + " of " + cardsColor.toString();
+        }
+
+    }
+
 }
 
