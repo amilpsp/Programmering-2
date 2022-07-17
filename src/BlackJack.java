0983ad394763b45d5ad1ea536d73e4d451e8e175
amilpsp <amanda.m.ruiz@gmail.com>
2022-07-17 02:26:51 +0200

    playersHand list replaced by drewAce boolean.

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -19,25 +19,46 @@ public class BlackJack {
         while (continuing){ //whole program play again loop
 
             switch(decideTurn()){
+
                 case 1:
                     boolean drawAnother=true;
                     boolean drewAce=false;
 
                     while(drawAnother) {
+                        if (allCardsQueue.isEmpty()){
+                            myDeck.generateAndShuffleDeck();
+                        }
                         PokerDeck.Card cardDrawn =Player.User.drawCard(allCardsQueue);
                         if (cardDrawn.toString().contains("Ace"))
                             drewAce=true;
-                        user.score += cardDrawn.getCardsValue();
-                        if (user.score>=21 && drewAce==true){
-                            user.score -= 13;
-
+                        Player.User.score += cardDrawn.getCardsValue();
+                        if (Player.User.score >= 21 && drewAce){
+                            Player.User.score -= 13;
+                            drewAce=false;
                         }
-
                         drawAnother = Player.User.yesOrNoQuestion("Do you want to draw another card from the deck?");
                     }
                     Player.User.yetToPlay=false;
                     break;
                 case 2:
+                    drawAnother=true;
+                    drewAce = false;
+
+                    while(drawAnother) {
+                        if (allCardsQueue.isEmpty()){
+                            myDeck.generateAndShuffleDeck();
+                        }
+                        PokerDeck.Card cardDrawn =Player.Computer.drawCard(allCardsQueue);
+                        if (cardDrawn.toString().contains("Ace"))
+                            drewAce=true;
+                        Player.Computer.score += cardDrawn.getCardsValue();
+                        if (Player.Computer.score >= 21 && drewAce){
+                            Player.Computer.score -= 13;
+                            drewAce=false;
+                        }
+                        drawAnother = Player.Computer.drawAnotherDecision(Player.Computer.score);
+                    }
+                    Player.Computer.yetToPlay=false;
                     break;
                 case 3:
                     break;
@@ -52,7 +73,32 @@ public class BlackJack {
         }//whole program play again loop
     }//main
 
+ /*
 
+-------------I want to find a "quick" way to not have this repeat, it's probably obvious and I'm------------------------
+-----------------------------------overcomplicating my thought process--------------------------------------------------
+
+    static void turn(Player player, Queue<PokerDeck.Card> allCardsQueue,PokerDeck.Deck myDeck){
+
+        boolean drawAnother=true;
+        boolean drewAce=false;
+
+        while(drawAnother) {
+            if (allCardsQueue.isEmpty()){
+                myDeck.generateAndShuffleDeck();
+            }
+            PokerDeck.Card cardDrawn =Player.User.drawCard(allCardsQueue);
+            if (cardDrawn.toString().contains("Ace"))
+                drewAce=true;
+            player.score += cardDrawn.getCardsValue();
+            if (player.score >= 21 && drewAce){
+                player.score -= 13;
+                drewAce=false;
+            }
+            drawAnother = Player.User.yesOrNoQuestion("Do you want to draw another card from the deck?");
+        }
+        Player.User.yetToPlay=false;
+    }*/
     static Queue<PokerDeck.Card> arrayListToQueue(PokerDeck.Deck myDeck){
         Queue<PokerDeck.Card> allCards = new LinkedList<>(Arrays.asList(myDeck.allCardsArrayList.toArray(new PokerDeck.Card[52])));
         return allCards;
@@ -73,8 +119,8 @@ public class BlackJack {
 }
 
 class Player{
-    static int score;
     static boolean yetToPlay = false;
+    public int score;
 
     static PokerDeck.Card drawCard(Queue<PokerDeck.Card> allCardsQueue){
         PokerDeck.Card cardDrawn = allCardsQueue.remove(); //drawing a card
@@ -82,10 +128,11 @@ class Player{
         return cardDrawn;
     }
 
-     static class User extends Player{
+      static class User extends Player{
          static int score;
          static boolean yetToPlay = false;
-         static LinkedList<String> playersHand = new LinkedList<>();
+
+      /*   static LinkedList<String> playersHand = new LinkedList<>();*/
         static boolean yesOrNoQuestion(String question){ //asks the user if they want to continue.
             char answer = 0;
             boolean validInput = false;
@@ -98,7 +145,7 @@ class Player{
                     if (answer == 'Y' || answer == 'N') {
                         validInput = true;
                         if (answer == 'N'){     // These 3 lines were the only
-                            yetToPlay = true;   // new aditions to the old
+/*why are these here?*/     yetToPlay = true;   // new additions to the old
                         }                       // code from last course
                     }
                     else {
@@ -113,9 +160,9 @@ class Player{
             }
             return answer != 'N';
         }
-        static List<String> getPlayersHand(){
+        /*static List<String> getPlayersHand(){
              return playersHand;
-         }
+         }*/
     }
 
     static class Computer extends Player{
