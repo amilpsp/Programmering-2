16ccff48a17a92c9247a12033afed83650042634
amilpsp <amanda.m.ruiz@gmail.com>
2022-07-17 23:54:01 +0200

    Made a new branch and deleted all of the other source files in the course project for the benefit of turning in the inlämningsuppgift nr7

--- src/U5_MainClassD.java
+++
@@ -1,7 +0,0 @@
-public class U5_MainClassD {
-    public static void main( String[] args) {
-        U5_ClassD cd = new U5_ClassD(1, 2, 3);
-        cd.modify(10, 10);
-        cd.printout();
-    }
-}
